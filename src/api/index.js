9c5234b7a80afc6cc518c5992fced7d8bd5b8d0d
khotsu <rutchanont_to66@live.rmutl.ac.th>
2024-03-06 10:03:26 +0700

    fix -6/3/23

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 export async function login({ email, password }) {
-  return await fetch("/api/auth/login", {
+  return await fetch("http://api-user66026.se-rmutl.net/api/auth/login", {
     method: "POST",
     body: JSON.stringify({ email, password }),
     headers: { "Content-Type": "application/json" },
@@ -18,10 +18,13 @@ export async function login({ email, password }) {
 }
 
 export async function searchArtworks({ keyword }) {
-  return await fetch(`/api/homepage/getArtworks/${keyword}`, {
-    method: "GET",
-    headers: { "Content-Type": "application/json" },
-  })
+  return await fetch(
+    `http://api-user66026.se-rmutl.net/api/homepage/getArtworks/${keyword}`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  )
     .then((response) => {
       // If request is not successful, display error message
       if (!response.ok) {
